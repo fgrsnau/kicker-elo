089a85edc47d45e1a85d6a0c374e4178b4c590bd
Stefan Haller <fgrsnau@gmail.com>
2019-12-04 23:02:38 +0100

    Refactor WebTokenStorage

--- web.go
+++ web.go
@@ -1,85 +1,12 @@
 package main
 
 import (
-	"crypto/rand"
-	"encoding/base32"
 	"encoding/json"
+	"log"
 	"net/http"
 	"reflect"
-	"sync"
 	"time"
 )
-
-type WebToken struct {
-	User      string
-	Timestamp time.Time
-}
-
-func NewWebToken(user string) WebToken {
-	return WebToken{
-		User:      user,
-		Timestamp: time.Now(),
-	}
-}
-
-func (t WebToken) Refresh() WebToken {
-	newToken := t
-	newToken.Timestamp = time.Now()
-	return newToken
-}
-
-type WebTokenStorage struct {
-	storage map[string]WebToken
-	mutex   sync.Mutex
-}
-
-func NewWebTokenStorage() WebTokenStorage {
-	return WebTokenStorage{
-		storage: make(map[string]WebToken),
-	}
-}
-
-func (w *WebTokenStorage) computeToken() string {
-	var tmp [32]byte
-	if _, err := rand.Read(tmp[:]); err != nil {
-		panic(err)
-	}
-	return base32.StdEncoding.EncodeToString(tmp[:])
-}
-
-func (w *WebTokenStorage) MapUser(user string) string {
-	for {
-		token := w.computeToken()
-		w.mutex.Lock()
-		if _, ok := w.storage[token]; !ok {
-			w.storage[token] = NewWebToken(user)
-			w.mutex.Unlock()
-			return token
-		}
-		w.mutex.Unlock()
-	}
-}
-
-func (w *WebTokenStorage) VerifyToken(token string) string {
-	w.mutex.Lock()
-	defer w.mutex.Unlock()
-	t, ok := w.storage[token]
-	if ok {
-		w.storage[token] = t.Refresh()
-		return t.User
-	}
-	return ""
-}
-
-func (w *WebTokenStorage) Expire() {
-	w.mutex.Lock()
-	defer w.mutex.Unlock()
-	for k, v := range w.storage {
-		if time.Since(v.Timestamp) > 24*time.Hour {
-			delete(w.storage, k)
-		}
-	}
-}
 
 var WebTokens WebTokenStorage = NewWebTokenStorage()
 
@@ -105,8 +32,7 @@ func webParseRequestAndVerifyToken(w http.ResponseWriter, r *http.Request, v int
 
 func webHandleLogin(w http.ResponseWriter, r *http.Request) {
 	var request struct {
-		User     string
-		Password string
+		User, Password string
 	}
 
 	var response struct {
@@ -303,7 +229,8 @@ func WebRun() {
 	go func() {
 		for {
 			time.Sleep(5 * time.Minute)
-			WebTokens.Expire()
+			count := WebTokens.Expire()
+			log.Printf("current web tokens count: %d", count)
 		}
 	}()
 
